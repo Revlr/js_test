6a10ade54f6e4560d4a4f08fa3fa7cb96e3eff2a
Revlr <jjh8501@gmail.com>
2021-02-12 23:50:24 +0900

    lect. 2-2 finished

--- 2/02/index.js
+++ 2/02/index.js
@@ -1,3 +1,7 @@
 const title = document.getElementById("title");
 
 console.log(title);
+//id 값을 getElementById("<id>")로 받아와서 변수에 DOM 객체로서 저장 후
+//innerHTML로 내용 변경 ㄱㄴ
+const test = document.getElementById("test");
+test.innerHTML = "Test7357";
