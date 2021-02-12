ddfbd297915da40160152274152c46a0991e3573
Revlr <jjh8501@gmail.com>
2021-02-12 22:17:03 +0900

    add directory and modify directory name

---
+++ 1/09/index.js
@@ -0,0 +1,11 @@
+//array 생성
+
+const daysOfWeek =  ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+console.log(daysOfWeek);
+console.log(daysOfWeek[3]);
+
+var v = "yee";
+const arr = ["string", 31337, true, v];
+
+console.log(arr);
