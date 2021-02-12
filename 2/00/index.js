35b6fa8a93d6826ebdb264acc2bf665188a68869
Revlr <jjh8501@gmail.com>
2021-02-12 22:36:15 +0900

    add index.js

---
+++ 2/00/index.js
@@ -0,0 +1,13 @@
+//function test
+
+function sayHello(potato){
+    console.log("Hello",potato+"!!");
+}
+
+friendsArray = ["sanghoon", "sungho", "taeho", 123123, true];
+
+for (let i = 0; i < friendsArray.length; i++) {
+    sayHello(friendsArray[i]);
+    
+}
+sayHello();
