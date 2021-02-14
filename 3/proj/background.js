c9298ce5b200ec424c4fdec8dbdb513c763f2fe2
Guest <guest-fizkhb@h26863.sgb.skir.kr>
2021-02-14 23:07:24 +0900

    add random image

--- 3/proj/background.js
+++ 3/proj/background.js
@@ -15,4 +15,13 @@ function getJSON(url) {
     return resp ;
 }
 
-var json = getJSON("https://picsum.photos/id/"+Math.floor((Math.random()*1000))+"/asdf/info");
+var json = getJSON("https://picsum.photos/id/"+Math.floor((Math.random()*100))+"/info");
+
+var parsedJson = JSON.parse(json);
+var w = parsedJson.width;
+var h = parsedJson.height;
+var id = parsedJson.id;
+var url = "https://picsum.photos/id/"+id+"/"+w+"/"+h;
+
+const bodyBackground = document.querySelector("body");
+bodyBackground.style = "background:url("+url+") no-repeat center/cover;";
