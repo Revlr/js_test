f03b60c66d87a0ff9488dd55f0749c9558968855
Guest <guest-2nbdcc@h26875.sgb.skir.kr>
2021-02-14 13:39:06 +0900

    finished 3-2

--- 3/proj/clock.js
+++ 3/proj/clock.js
@@ -5,10 +5,12 @@ function getTime(){
     const date = new Date();
     const min = date.getMinutes();
     const hour = date.getHours();
-    clockTitle.innerText = `${hour}:`+`0${min}`.slice(-2);
+    const sec = date.getSeconds();
+    clockTitle.innerText = `0${hour}`.slice(-2)+":"+`0${min}`.slice(-2)+":"+`0${sec}`.slice(-2);
 }
 
 function init(){
     getTime();
+    setInterval(getTime, 1000)
 }
 init();
