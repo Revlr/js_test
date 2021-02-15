d9d7d7ea60dba804e9f5422b67f2eb8d5b2567ea
Revlr <jjh8501@gmail.com>
2021-02-15 20:11:40 +0900

    asdf

--- 3/proj/test.js
+++ 3/proj/test.js
@@ -1,16 +1,24 @@
-function saveToFile_Chrome(fileName, content) {
-    var blob = new Blob([content], { name: fileName, type: 'text/plain' });
-    objURL = window.URL.createObjectURL(blob);
+// function saveToFile_Chrome(fileName, content) {
+//     var blob = new Blob([content], { type: 'text/plain' });
+//     var objURL = window.URL.createObjectURL(blob);
             
-    // 이전에 생성된 메모리 해제
-    if (window.__Xr_objURL_forCreatingFile__) {
-        window.URL.revokeObjectURL(window.__Xr_objURL_forCreatingFile__);
-    }
-    window.__Xr_objURL_forCreatingFile__ = objURL;
-    var a = document.createElement('a');
-    a.download = fileName;
-    a.href = objURL;
-    a.click();
-}
+//     // 이전에 생성된 메모리 해제
+//     if (window.__Xr_objURL_forCreatingFile__) {
+//         window.URL.revokeObjectURL(window.__Xr_objURL_forCreatingFile__);
+//     }
+//     window.__Xr_objURL_forCreatingFile__ = objURL;
+//     var a = document.createElement('a');
+//     a.download = fileName;
+//     a.href = objURL;
+//     a.click();
+// }
 
-saveToFile_Chrome("./show.txt", "asdf");
+// saveToFile_Chrome("~/js_test/3/proj/todolist/show.txt", "asdf");
+
+//이건 ㄹㅇ 파일 저장이었음;;
+
+var blob = new Blob(["test"]);
+window.navigator.msSaveBlob(blob, "testFile.txt");
+var fileData = ["test data test data"];
+blob = new Blob(fileData);
+window.navigator.msSaveOrOpenBlob(blob, "testFile2.txt");
