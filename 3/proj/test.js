1b11c4209d50d4186fb22d486f87f93ee2238e0b
Revlr <jjh8501@gmail.com>
2021-02-15 19:48:56 +0900

    add test file

---
+++ 3/proj/test.js
@@ -0,0 +1,16 @@
+function saveToFile_Chrome(fileName, content) {
+    var blob = new Blob([content], { name: fileName, type: 'text/plain' });
+    objURL = window.URL.createObjectURL(blob);
+            
+    // 이전에 생성된 메모리 해제
+    if (window.__Xr_objURL_forCreatingFile__) {
+        window.URL.revokeObjectURL(window.__Xr_objURL_forCreatingFile__);
+    }
+    window.__Xr_objURL_forCreatingFile__ = objURL;
+    var a = document.createElement('a');
+    a.download = fileName;
+    a.href = objURL;
+    a.click();
+}
+
+saveToFile_Chrome("./show.txt", "asdf");
